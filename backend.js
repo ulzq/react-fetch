3fcf7d9e8960c7ad219bbebdd22ebc8d631094f5
anx <sebastian.glaser@digitalcareerinstitute.org>
2020-02-06 09:33:16 +0100

    boilerplate

---
+++ backend.js
@@ -0,0 +1,33 @@
+
+const express = require('express')
+const app     = express()
+
+const session = require('express-session')
+
+app.use( session({
+  resave: true,
+  saveUninitialized: true,
+  secret: '1-2987921837b109287301827012873v78bds8a7d6asdashhdgv',
+  cookie: {}
+}) );
+
+app.get( '/say',(req,res)=> {
+  res.json(req.body);
+});
+
+app.get('/set',(req,res)=>{
+  req.session.counter = req.session.counter + 1;
+  req.session.data = req.body;
+  res.json(req.session.counter);
+});
+
+app.get('/get',(req,res)=>{
+  res.json(req.session.data);
+});
+
+app.get('/del',(req,res)=>{
+  req.session.destroy();
+  res.json('ok');
+});
+
+app.listen(9933);
